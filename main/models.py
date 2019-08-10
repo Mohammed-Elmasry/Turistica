be372c5e95c0c63ff205f4f4119d35e98e03f9ef
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-08-11 01:51:40 +0200

    create Trip model in  models file

--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Trip(models.Model):
+    trip_id = models.IntegerField(auto_created=True ,primary_key=True)
+    trip_number = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    status_is_active = models.BooleanField(default=True)
+    total_cost = models.DecimalField(max_digits=6)
+    hours = models.IntegerField()
+    extra_hours = models.FloatField()
+    payment_way = [('Cash','Cash'),('Visa','Visa')]
+    creation_date = models.DateTimeField(auto_now= True)
+
+
+
+
+
+
+
+
