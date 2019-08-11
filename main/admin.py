f6b56c3b60e5018f90e97432e690264604b051a1
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-08-11 05:22:36 +0200

    create classes for admin in admin file

--- main/admin.py
+++ main/admin.py
@@ -16,6 +16,32 @@ admin.site.register(models.Site , SiteAdmin)
 class TouristAdmin(admin.ModelAdmin):
     list_display = ['tourist_first_name','email','date_of_join']
     search_fields = ['tourist_first_name','tourist_last_name','email']
-    list_filter = ['tourist_first_name','tourist_last_name','email','date_of_join']
-    exclude = ['password']#it hide fields in admin form we will add all this fields in the future
+    list_filter = ['date_of_join']
+    exclude = ['password','wallet']#it hide fields in admin form we will add all this fields in the future
 admin.site.register(models.Tourist,TouristAdmin)
+
+class TransportationAdmin(admin.ModelAdmin):
+    list_display = ['trans_type']
+    search_fields = ['trans_type']
+admin.site.register(models.Transportation,TransportationAdmin)
+
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ['language_name']
+    search_fields = ['language_name']
+admin.site.register(models.Language,LanguageAdmin)
+
+class GuideAdmin(admin.ModelAdmin):
+    list_display = ['guide_first_name','guide_last_name','guide_per_hour','date_of_join']
+    exclude = ['password','wallet']
+    list_filter = ['date_of_join']
+    search_fields = ['guide_first_name','guide_last_name','language'
+        ,'national_id','licence_id','date_of_join']
+admin.site.register(models.Guide,GuideAdmin)
+
+class TripAdmin(admin.ModelAdmin):
+    list_display = ['trip_id','trip_number','start_date','end_date',
+                    'start_time','end_time','creation_date']
+    list_filter = ['creation_date','category','city','transportation','guide','site']
+    search_fields = ['trip_id','trip_number',
+                     'creation_date','category','city','transportation','guide']
+admin.site.register(models.Trip,TripAdmin)
--- main/models.py
+++ main/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 
 class Category(models.Model):
+    def __str__(self):
+        return self.category_name
+
     category_id = models.AutoField(primary_key=True)
     category_name = models.CharField(blank=True, max_length=32)
 
 class City(models.Model):
+    def __str__(self):
+        return self.city_name
+
     city_id = models.AutoField(primary_key=True)
     city_name = models.CharField(max_length=32,blank=True)
 
 class Site(models.Model):
+    def __str__(self):
+        return self.site_name
+
     city = models.ForeignKey(City,on_delete=models.CASCADE)
     site_id = models.AutoField(primary_key=True)
     site_name = models.CharField(blank=True, max_length=32)
@@ -18,6 +27,9 @@ class Site(models.Model):
 
 
 class Tourist(models.Model):
+    def __str__(self):
+        return self.tourist_first_name + self.tourist_last_name
+
     tourist_id = models.AutoField(primary_key=True)
     tourist_first_name = models.CharField(max_length=32, blank=True, null=False)
     tourist_last_name = models.CharField(max_length=32, blank=True,null=False)
@@ -25,36 +37,46 @@ class Tourist(models.Model):
     email = models.EmailField(blank=True)
     password = models.CharField(max_length=50)
     date_of_join = models.DateField(auto_now=True)
-    wallet = models.DecimalField(max_digits=6, decimal_places=2)
+    wallet = models.DecimalField(max_digits=6, decimal_places=2,default=0.0)
 
 
 class Transportation(models.Model):
-    trans_id = models.AutoField(primary_key=True)
-    trans_type = [('Public Transportation', 'public'), ('Private Transportation', 'private')]
+    def __str__(self):
+        return self.trans_type
 
+    trans_id = models.AutoField(primary_key=True)
+    TRANS = [('Public Transportation', 'public'), ('Private Transportation', 'private')]
+    trans_type = models.CharField(choices=TRANS,max_length=30,default='private')
 
 class Language(models.Model):
+    def __str__(self):
+        return self.language_name
+
     language_id = models.AutoField(primary_key=True)
-    language_name = [('English', 'English'), ('Arabic', 'Arabic'),
+    LANGUAGES = [('English', 'English'), ('Arabic', 'Arabic'),
                      ('German', 'German'), ('Italian', 'Italian'),
                      ('Russian', 'Russian'), ('Turkish', 'Turkish'),
                      ('Swedish', 'Swedish'), ('Greek', 'Greek'),
                      ('French', 'French')]
+    language_name =models.CharField(choices=LANGUAGES,max_length=30,default='English')
 
 class Guide(models.Model):
-    language = models.ManyToManyField(Language,blank=True)
+    def __str__(self):
+        return 'Guide' + self.guide_first_name
+
     guide_id = models.AutoField(primary_key=True)
     guide_first_name = models.CharField(max_length=32, blank=True,null=False)
     guide_last_name = models.CharField(max_length=32, blank=True,null=False)
     email = models.EmailField(blank=True)
+    language = models.ManyToManyField(Language, blank=True)
     password = models.CharField(max_length=50)
     date_of_birth = models.DateField(null=False)
     bank_account = models.CharField(null=False,max_length=30)
-    wallet = models.DecimalField(max_digits=6, decimal_places=2)
+    wallet = models.DecimalField(max_digits=6, decimal_places=2,default=0.0)
     guide_per_hour = models.DecimalField(max_digits=6,null=False,decimal_places=2)
     national_id = models.CharField(max_length=13,null=False)
     licence_id = models.CharField(null=False,max_length=30)  # we need to know the max_length of licence_id
-
+    date_of_join = models.TimeField(auto_now=True)
 
 class GuidePhotoData(models.Model):
     guide = models.OneToOneField(Guide,on_delete=models.CASCADE,primary_key=True)
@@ -75,8 +97,8 @@ class Trip(models.Model):
     trip_number = models.IntegerField()
     start_date = models.DateField()
     end_date = models.DateField()
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
     status_is_active = models.BooleanField(default=True)
     total_cost = models.DecimalField(max_digits=6,decimal_places=2)
     hours = models.IntegerField()
