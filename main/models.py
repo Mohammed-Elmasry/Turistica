6efed40fd4ea7d116c86bfc7c50ce5d47b3846c7
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-08-11 02:29:16 +0200

    create Guide model in models file

--- main/models.py
+++ main/models.py
@@ -34,7 +34,7 @@ class Tourist(models.Model):
     email = models.EmailField(blank=True)
     password = models.CharField(max_length=50)
     date_of_join = models.DateField(auto_now=True)
-    wallet = models.DecimalField(max_length=6)
+    wallet = models.DecimalField(max_length=6,decimal_places=None)
 
 class City(models.Model):
     city_id = models.IntegerField(primary_key=True,auto_created=True)
@@ -44,4 +44,17 @@ class City(models.Model):
 class Transportation(models.Model):
     trans_id = models.IntegerField(primary_key=True,auto_created=True)
     trans_type = [('Public Transportation','public'),('Private Transportation','private')]
+
+class Guide(models.Model):
+    guide_id = models.IntegerField(primary_key=True , auto_created=True)
+    guide_first_name = models.CharField(max_length=32,blank=True)
+    guide_last_name = models.CharField(max_length=32 ,blank=True)
+    email = models.EmailField(blank=True)
+    password = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    bank_account = models.CharField()
+    wallet = models.DecimalField(max_digits=6 ,decimal_places=None)
+    guide_per_hour = models.DecimalField(max_digits=6)
+    national_id = models.CharField(max_length=13)
+    licence_id = models.CharField()#we need to know the max_length of licence_id
 
