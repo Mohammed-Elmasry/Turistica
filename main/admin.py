558e15e18db7508fcc3f4ff9a70f9e3c0dec186e
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-08-11 04:09:41 +0200

    create class for siteAdmin in admin file

--- main/admin.py
+++ main/admin.py
@@ -5,3 +5,10 @@ class CityAdmin(admin.ModelAdmin):
     list_display = ['city_name']
     search_fields = ['city_name']
 admin.site.register(models.City,CityAdmin)
+
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ['site_name','open_from','open_to','site_cost']
+    search_fields = ['site_name']
+    list_filter = ['site_name','open_from','open_to','site_cost']
+    list_editable = ['open_from','open_to']
+admin.site.register(models.Site , SiteAdmin)
