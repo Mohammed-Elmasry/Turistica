e9aa204370f586db51b267cd805a4d033784059e
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-08-11 03:44:03 +0200

    create cityAdmin class in admin file

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('city_name')
+    search_fields = ('city_name')
+admin.site.register(models.City,CityAdmin)
